130fe0dc6e5d0af093e97f756ba38ae767944bf3
root <root@Lucas>
2023-11-09 23:09:14 -0300

    Home page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import Home from "./components/Home"
 
 function App() {
 
@@ -7,6 +8,8 @@ function App() {
     <>
 
       <Header />
+
+      <Home />
       
     </>
   )
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,64 @@
+import "./styles.css"
+
+import icon from "../../assets/Icons/Dev.svg"
+
+import github from "../../assets/Icons/Github.svg"
+import instagram from "../../assets/Icons/Instagram.svg"
+import linkedin from "../../assets/Icons/Linkedin.svg"
+
+
+export default function Home() {
+
+    return (
+        <>
+            <section className="ptf-home-section">
+                <div className="ptf-heigh">
+                    <div className="ptf-center">
+                        <div className="ptf-organization ptf-ajustation ">
+                            <div className="ptf-description">
+                                <div className="ptf-details">
+
+                                    <p className="ptf-presentations">
+                                        <span className="ptf-name"
+                                        >Lucas Fernandes</span>
+                                        <br />
+                                        <span className="ptf-tech">
+                                            Desenvolvedor Back-end.
+                                        </span>
+
+                                    </p>
+
+                                    <div className="ptf-icons-social">
+                                        <a href="https://www.linkedin.com/in/lucas-fernandes-botelho/"
+                                            target="_blank">
+                                            <img src={linkedin} alt="" />
+                                        </a>
+
+                                        <a href="https://www.github.com/lfernandex"
+                                            target="_blank">
+                                            <img src={github} alt="" />
+                                        </a>
+
+
+
+                                        <a href="https://instagram.com/lfernande_s"
+                                            target="_blank">
+                                            <img src={instagram} alt="" />
+                                        </a>
+
+                                    </div>
+
+                                </div>
+                                <div className="ptf-social-media">
+                                    <img src={icon} alt="" />
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
